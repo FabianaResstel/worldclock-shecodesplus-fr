d1e733c360629db7725fdd478cfc087d40b8c52c
FabianaResstel <fabianaresstel01@gmail.com>
2026-05-29 17:45:25 -0400

    add interval and update js

--- src/script.js
+++ src/script.js
@@ -1,17 +1,19 @@
-let cityElement = document.querySelector("#los-angeles");
-let cityDateElement = cityElement.querySelector(".cityDate");
-let cityTimeElement = cityElement.querySelector(".cityTime");
-cityDateElement.innerHTML = moment
-  .tz("America/Los_Angeles")
-  .format("MMMM  Do  YYYY");
+function updateTime() {
+  let cityElement = document.querySelector("#los-angeles");
+  let cityDateElement = cityElement.querySelector(".cityDate");
+  let cityTimeElement = cityElement.querySelector(".cityTime");
+  let cityMoment = moment.tz("America/Los_Angeles");
+  cityDateElement.innerHTML = moment().format("MMMM  Do  YYYY");
 
-cityTimeElement.innerHTML = moment.tz("America/Los_Angeles").format(`h:mm:ss`);
+  cityTimeElement.innerHTML = moment
+    .tz("America/Los_Angeles")
+    .format(`h:mm:ss`);
 
-let secCityElement = document.querySelector("#paris");
-let secCityDateElement = secCityElement.querySelector(".cityDate");
-let secCityTimeElement = secCityElement.querySelector(".cityTime");
-
-secCityDateElement.innerHTML = moment
-  .tz("Europe/Paris")
-  .format("MMMM  Do  YYYY");
-secCityTimeElement.innerHTML = moment.tz("Europe/Paris").format(`h:mm:ss`);
+  let secCityElement = document.querySelector("#paris");
+  let secCityDateElement = secCityElement.querySelector(".cityDate");
+  let secCityTimeElement = secCityElement.querySelector(".cityTime");
+  let secCityMoment = moment.tz("Europe/Paris");
+  secCityDateElement.innerHTML = moment().format("MMMM  Do  YYYY");
+  secCityTimeElement.innerHTML = moment.tz("Europe/Paris").format(`h:mm:ss`);
+}
+setInterval(updateTime, 1000);
